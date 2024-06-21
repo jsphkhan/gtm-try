ad57d8c8bb0247dd9c139216f46c96a776614709
Joseph Khan <jsphkhan@gmail.com>
2024-06-21 10:46:35 +0400

    add gtm

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,25 +1,42 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
+        {/* <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-P2SJ37K8');
           `
-        }} />
+        }}></script> */}
       </Head>
       <body>
         <Main />
         <NextScript />
+        <Script
+          src="https://www.googletagmanager.com/gtm.js?id=GTM-P2SJ37K8"
+          strategy="afterInteractive"
+          onReady={() => {
+            console.log('**** GTM Loaded ****');
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+              event: 'gtm.js',
+              'gtm.start': new Date().getTime()
+            });
+          
+          }}
+        />
         {/* GTM No Script */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2SJ37K8"
-          height="0" width="0" style={{display: 'none', visibility: 'hidden' }}></iframe></noscript>
+        {/* <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2SJ37K8"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }}></noscript> */}
         {/* End GTM No Script */}
       </body>
     </Html>
