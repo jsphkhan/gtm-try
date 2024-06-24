b44db542041adb6ffd92c1be935f35195a75d2a3
Joseph Khan <jsphkhan@gmail.com>
2024-06-24 13:27:43 +0400

    added comments

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import { GoogleTagManager } from '@next/third-parties/google'
 
+/**
+ * Tag Manager Integrated from 
+ * https://tagmanager.google.com/#/container/accounts/6236365334/containers/188129135/workspaces/3 
+ */
 export default function App({ Component, pageProps }) {
   return (
     <>
