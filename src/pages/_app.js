83e07091cc88b6bbec5c52f8bb33ad86b2f63181
Joseph Khan <jsphkhan@gmail.com>
2024-06-24 12:37:18 +0400

    fix: use gtm component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import "@/styles/globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleTagManager gtmId="GTM-P2SJ37K8" />
+    </>
+  );
 }
